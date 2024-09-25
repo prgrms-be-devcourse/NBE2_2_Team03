972c93cde32b77ea884c9b9a858433afddf5e75b
jooinjoo <jooinjoo0@gmail.com>
2024-09-26 04:11:57 +0900

    refactor: 관심목록 엔티티의 회원-청원 관계 설정 #9

--- src/main/java/com/example/nbe2_2_team03/entity/Interest.java
+++ src/main/java/com/example/nbe2_2_team03/entity/Interest.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 @Entity
 @Table(name = "interest")
+@ToString
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,8 +17,11 @@ public class Interest {
     @Column(name = "interest_id", nullable = false, unique = true)
     private Long interestId;
 
+    @ManyToOne
+    @JoinColumn(name = "petition_id", nullable = false)
+    private Petition petition;  // 청원 필드 추가. N:1 연결
 
     @ManyToOne
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 }
