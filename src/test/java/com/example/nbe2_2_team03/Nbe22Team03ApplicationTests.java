589469b4ec57480a90e6f1df6c929b925c992a69
jooinjoo <jooinjoo0@gmail.com>
2024-09-25 13:04:42 +0900

    feat: 프로젝트 설정

---
+++ src/test/java/com/example/nbe2_2_team03/Nbe22Team03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbe2_2_team03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe22Team03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
