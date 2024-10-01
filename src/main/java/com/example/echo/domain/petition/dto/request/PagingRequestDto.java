ad063c278d0bb2a7824eca7915299f9eddb1b55b
kimdongjoon-web <color6763@naver.com>
2024-10-02 07:27:38 +0900

    feat: 좋아요순, 만료일순, 카테고리별 정렬 구현, 관련 테스트 추가

--- src/main/java/com/example/echo/domain/petition/dto/request/PagingRequestDto.java
+++ src/main/java/com/example/echo/domain/petition/dto/request/PagingRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.echo.domain.petition.dto.request;
 
+import com.example.echo.domain.petition.entity.Category;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
@@ -11,11 +12,12 @@ import org.springframework.data.domain.Sort;
 public class PagingRequestDto {
     private int page = 0;
     private int size = 10;
-    private String sortBy = "agreeCount";
+    private SortBy sortBy = SortBy.AGREE_COUNT;
     private String direction = "desc"; // 동의자 순 나열
+    private Category category;
 
     public Pageable toPageable() {
         Sort.Direction sortDirection = Sort.Direction.fromString(direction);
-        return PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
+        return PageRequest.of(page, size, Sort.by(sortDirection, sortBy.getField()));
     }
 }
