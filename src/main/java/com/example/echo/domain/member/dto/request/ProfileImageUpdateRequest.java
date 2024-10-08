8d07499a10f405f8fb170f62f26fc4a6c94cb30e
jooinjoo <jooinjoo0@gmail.com>
2024-10-08 12:29:54 +0900

    feat: File 컨트롤러 및 DTO Swagger 추가 #125

--- src/main/java/com/example/echo/domain/member/dto/request/ProfileImageUpdateRequest.java
+++ src/main/java/com/example/echo/domain/member/dto/request/ProfileImageUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.example.echo.domain.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Validated
 public class ProfileImageUpdateRequest {
 
+    @Schema(description = "업데이트할 프로필 이미지 파일", required = true)
     @NotNull(message = "파일은 비어있을 수 없습니다.")
     private MultipartFile avatarImage;
 }
